03cd0c58974126d8088618ba5fb0c88c025c70fa
Andrew Rice <abrice@edanalytics.org>
2017-05-09 09:54:24 -0500

    added workspace call

--- R/get.R
+++ R/get.R
@@ -33,8 +33,15 @@ get_current <- function(api_token=get_toggl_api_token()){
   
 }
 
-get_workspaces<-function(){
-content(GET("https://www.toggl.com/api/v8/workspaces",     verbose(),
-    authenticate(api_token,"api_token"),
-    encode="json"))
-  }
+
+
+# get_current <- function(api_token=get_toggl_api_token()){
+#   if (is.null(api_token)){
+#     stop("you have to set your api token using options(toggl_api_token = 'XXXXXXXX')")
+#     
+#   }
+#   content(GET("https://www.toggl.com/api/v8/time_entries/current",
+#               # verbose(),
+#               authenticate(api_token,"api_token"),
+#               encode="json"))$data
+# }
